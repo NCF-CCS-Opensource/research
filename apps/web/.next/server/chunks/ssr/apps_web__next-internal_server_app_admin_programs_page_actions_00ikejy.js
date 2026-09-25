cee63fb76dc3132219822c93547cf56ddeb4f29a
Jai Infante <jeromeinfante.dev@gmail.com>
2026-09-25 08:44:22 +0800

    chore: stop tracking build output and dependency shims
    Update .gitignore to cover build output, turbo task logs, and node_modules across workspaces. Untrack .next, .turbo, and node_modules from git index without deleting local files. Closes #89

--- apps/web/.next/server/chunks/ssr/apps_web__next-internal_server_app_admin_programs_page_actions_00ikejy.js
+++
@@ -1,3 +0,0 @@
-module.exports=[55755,a=>{"use strict";a.s([])}];
-
-//# sourceMappingURL=apps_web__next-internal_server_app_admin_programs_page_actions_00ikejy.js.map
--- apps/web/.next/server/chunks/ssr/apps_web__next-internal_server_app_admin_research_page_actions_0rlc.gq.js
+++
@@ -1,3 +0,0 @@
-module.exports=[57358,a=>{"use strict";a.s([])}];
-
-//# sourceMappingURL=apps_web__next-internal_server_app_admin_research_page_actions_0rlc.gq.js.map
